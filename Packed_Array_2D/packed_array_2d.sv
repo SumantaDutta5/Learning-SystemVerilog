812683e9731c237741d7bc36ab6d27970d7805bb
Sumanta Kumar Dutta <84955195+SumantaDutta5@users.noreply.github.com>
2026-02-02 12:43:32 +0530

    Update packed_array_2d.sv

--- Packed_Array_2D/packed_array_2d.sv
+++ Packed_Array_2D/packed_array_2d.sv
@@ -2,11 +2,11 @@ module tb;
   bit [3:0] [7:0] m_data;
   
   initial begin
-  //1. Assign a value to the MDA 8
+  //1. Assign a value to the MDA
   m_data = 32'hface_cafe;
   $display("m_data=0x%0h", m_data);
     
-  //2. Iterate through each bit of the vector and print the values
+  //2. Iterate through each segment of the MDA and print the values
   for(int i = 0; i < $size(m_data); i++) begin
     $display("m_data[%d]=%b (0x%0h)",i,m_data[i], m_data[i]);
   end
