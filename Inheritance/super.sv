f3e9ef31420b74afbe7ce25d0bf56cb112d757be
Sumanta Kumar Dutta <84955195+SumantaDutta5@users.noreply.github.com>
2026-03-05 17:50:40 +0530

    Add parent and child classes with display methods

---
+++ Inheritance/super.sv
@@ -0,0 +1,30 @@
+class parent_trans;
+  bit [31:0] data;
+ 
+  function void display();
+    $display("Base value of data = %0h", data);
+  endfunction
+endclass
+
+
+class child_trans extends parent_trans;
+  bit [31:0] data;
+ 
+  function void display();
+    super.data = 3;
+    super.display();
+    $display("Child value of data = %0h", data);
+  endfunction
+endclass
+
+
+
+module class_example;
+  initial begin
+    child_trans c_tr;
+    c_tr = new();
+ 
+    c_tr.data = 5;
+    c_tr.display();
+  end
+endmodule
