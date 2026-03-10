38f519aad7e5a751c25348e2948a048142d4df0b
Sumanta Kumar Dutta <84955195+SumantaDutta5@users.noreply.github.com>
2026-03-10 12:43:43 +0530

    Add packet class with address range constraint

---
+++ Constraints/implications.sv
@@ -0,0 +1,24 @@
+class packet;
+  rand bit [3:0] addr;
+  string addr_range;
+  constraint address_range {(addr_range == "small") -> (addr < 8);}
+endclass
+
+module tb;
+  initial begin
+    packet pkt;
+    pkt = new();
+    
+    pkt.addr_range = "small";
+    repeat(3) begin
+      pkt.randomize();
+      $display("\taddr_range = %s	addr = %0d",pkt.addr_range,pkt.addr);
+    end
+    
+    pkt.addr_range = "high";
+    repeat(3) begin
+      pkt.randomize();
+      $display("\taddr_range = %s	addr = %0d",pkt.addr_range,pkt.addr);
+    end
+  end
+endmodule
